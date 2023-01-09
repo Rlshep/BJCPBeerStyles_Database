34edba8d3c6c93b1880e68b6037539769c5cfe93
Richard Sheppard <BeerStyles2015@gmail.com>
2023-01-08 21:38:51 -0500

    add ability to restrict languages

--- src/io/github/rlshep/bjcp2015beerstyles/db/CreateBjcpDatabase.java
+++ src/io/github/rlshep/bjcp2015beerstyles/db/CreateBjcpDatabase.java
@@ -1,6 +1,7 @@
 package io.github.rlshep.bjcp2015beerstyles.db;
 
 import io.github.rlshep.bjcp2015beerstyles.domain.Category;
+import org.apache.commons.lang.StringUtils;
 
 import java.io.IOException;
 import java.sql.Connection;
@@ -31,44 +32,19 @@ public class CreateBjcpDatabase {
         Statement stmt = null;
         BjcpDao bjcpDao = new BjcpDao();
         LoadDomainFromXML loadDomainFromXML = new LoadDomainFromXML();
-        List<Category> categories = new ArrayList<>();
+        boolean options = getOptions(args);
 
         try {
             Class.forName("org.sqlite.JDBC");
             c = DriverManager.getConnection("jdbc:sqlite:db/" + DATABASE_NAME);
             c.setAutoCommit(false);
             stmt = c.createStatement();
-
             bjcpDao.setDatabaseVersion(stmt);
             bjcpDao.createTables(stmt);
-
-            // Load database from xml file.
-            categories.addAll(loadDomainFromXML.loadXmlFromFile(BJCP_BEER_EN_2015, ENGLISH));
-            categories.addAll(loadDomainFromXML.loadXmlFromFile(BJCP_BEER_ES_2015, SPANISH));
-            categories.addAll(loadDomainFromXML.loadXmlFromFile(BJCP_BEER_UK_2015, UKRANIAN));
-
-            categories.addAll(loadDomainFromXML.loadXmlFromFile(BJCP_BEER_EN_2021, ENGLISH));
-            categories.addAll(loadDomainFromXML.loadXmlFromFile(BJCP_BEER_EN_2021, SPANISH));
-            categories.addAll(loadDomainFromXML.loadXmlFromFile(BJCP_BEER_UK_2021, UKRANIAN));
-
-            categories.addAll(loadDomainFromXML.loadXmlFromFile(BJCP_MEAD_EN_2015, ENGLISH, BJCP_2015));
-            categories.addAll(loadDomainFromXML.loadXmlFromFile(BJCP_MEAD_UK_2015, UKRANIAN, BJCP_2021));
-            categories.addAll(loadDomainFromXML.loadXmlFromFile(BJCP_MEAD_EN_2015, ENGLISH, BJCP_2021));
-            categories.addAll(loadDomainFromXML.loadXmlFromFile(BJCP_MEAD_UK_2015, UKRANIAN, BJCP_2021));
-
-            categories.addAll(loadDomainFromXML.loadXmlFromFile(BJCP_CIDER_EN_2015, ENGLISH, BJCP_2015));
-            categories.addAll(loadDomainFromXML.loadXmlFromFile(BJCP_CIDER_EN_2015, ENGLISH, BJCP_2021));
-
-            categories.addAll(loadDomainFromXML.loadXmlFromFile(BA_BEER_EN_2021, ENGLISH));
-            categories.addAll(loadDomainFromXML.loadXmlFromFile(BA_BEER_EN_2021, SPANISH));
-            categories.addAll(loadDomainFromXML.loadXmlFromFile(BA_BEER_EN_2021, UKRANIAN));
-
-            bjcpDao.addCategories(stmt, categories, -1);
+            bjcpDao.addCategories(stmt, getCategoriesFromXml(loadDomainFromXML, options), -1);
             bjcpDao.addMetaData(stmt);
-
             loadSqlFiles(bjcpDao, stmt, SYNONYM_FILE_NAME);
             loadSqlFiles(bjcpDao, stmt, FTS_FILE_NAME);
-
             c.commit();
         } catch (Exception e) {
             e.printStackTrace();
@@ -80,6 +56,42 @@ public class CreateBjcpDatabase {
                 System.err.println(e.getClass().getName() + ": " + e.getMessage());
             }
         }
+    }
+
+    private static boolean getOptions(String[] args) {
+        boolean englishOnly = false;
+
+        if (args.length > 0 && ENGLISH.equalsIgnoreCase(args[0])) {
+            englishOnly = true;
+        } else if (args.length > 0) {
+            System.out.println("Usage: ./gradlew loadDb Optional: -PappArgs=\"['en']\"");
+        }
+
+        return englishOnly;
+    }
+
+    private static List<Category> getCategoriesFromXml(LoadDomainFromXML loadDomainFromXML, boolean englishOnly) throws Exception {
+        List<Category> categories = new ArrayList<>();
+
+        categories.addAll(loadDomainFromXML.loadXmlFromFile(BJCP_BEER_EN_2015, ENGLISH));
+        categories.addAll(loadDomainFromXML.loadXmlFromFile(BJCP_BEER_EN_2021, ENGLISH));
+        categories.addAll(loadDomainFromXML.loadXmlFromFile(BJCP_MEAD_EN_2015, ENGLISH, BJCP_2015));
+        categories.addAll(loadDomainFromXML.loadXmlFromFile(BJCP_MEAD_EN_2015, ENGLISH, BJCP_2021));
+        categories.addAll(loadDomainFromXML.loadXmlFromFile(BJCP_CIDER_EN_2015, ENGLISH, BJCP_2015));
+        categories.addAll(loadDomainFromXML.loadXmlFromFile(BJCP_CIDER_EN_2015, ENGLISH, BJCP_2021));
+        categories.addAll(loadDomainFromXML.loadXmlFromFile(BA_BEER_EN_2021, ENGLISH));
+
+        if (!englishOnly) {
+            categories.addAll(loadDomainFromXML.loadXmlFromFile(BJCP_BEER_ES_2015, SPANISH));
+            categories.addAll(loadDomainFromXML.loadXmlFromFile(BJCP_BEER_EN_2021, SPANISH));
+            categories.addAll(loadDomainFromXML.loadXmlFromFile(BA_BEER_EN_2021, SPANISH));
+
+            categories.addAll(loadDomainFromXML.loadXmlFromFile(BJCP_BEER_UK_2015, UKRANIAN));
+            categories.addAll(loadDomainFromXML.loadXmlFromFile(BJCP_BEER_UK_2021, UKRANIAN));
+//            categories.addAll(loadDomainFromXML.loadXmlFromFile(BJCP_MEAD_UK_2015, UKRANIAN, BJCP_2021));
+        }
+
+        return categories;
     }
 
     private final static void loadSqlFiles(BjcpDao bjcpDao, Statement stmt, String fileName) {
